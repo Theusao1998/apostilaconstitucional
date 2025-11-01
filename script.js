e37b94ab98e170733914109f8888f4f57e635929
github <matt.hen98@gmail.com>
2025-10-31 21:11:55 -0300

    Add scroll-to-top button functionality

--- script.js
+++ script.js
@@ -87,3 +87,17 @@ function setupScrollSpy() {
         lastSectionObserver.observe(lastSection);
     }
 }
+// Botão "Voltar ao topo"
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    scrollTopBtn.style.display = "flex";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
